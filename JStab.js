7f6ea4716d17d3af21d13c287f041c03e09b20a3
Roseinfire <roseinfire.dev@gmail.com>
2023-02-08 20:42:04 +0300

    Add files via upload

--- JStab.js
+++ JStab.js
@@ -1,48 +1,53 @@
-function JStab(area, gap, left="") {   
-      function gotab(s) {
-            var tab = ""
-            for(var i = 1; i < s; i++) {
-                  tab += " "
-                  }
-            return left + tab
-            }     
-      var val = area.value
-      var res = ""
-      var stage = 1
-      var intext = false
-      var inht = false
-      var incomment = false
-      for(var i = 0; i < val.length; i++) {
+function JStab(area, gap, left=2) {   
+        var val = area.value
+        var res = ""
+        var stage = 1
+        var quote = 1
+        var intext = false
+        var inht = false
+        var incomment = false
+        for(var i = 0; i < val.length; i++) {
             var com = null
-    
             if(val[i] == "/" && val[i+1] == "*") { incomment = "long" }
             else if(val[i] == "/" && val[i+1] == "/") { incomment = "short" }    
             if(val[i] == "*" && val[i+1] == "/" && incomment == "long") { incomment = false }
-            else if(val[i] == "\n" && incomment == "short") { incomment = false }   
-    
+            else if(val[i] == "\n" && incomment == "short") { incomment = false }    
             if( !incomment && (val[i] == "'" || val[i] == '"' || val[i] == "`") && !intext) { intext = val[i] }
             else if(intext == val[i]) { intext = false }
             if( val[i] == ">") { inht=true }
             else if( val[i] == "<" ) { inht = false }
-            if(val[i] == "{" && !intext && !incomment) { stage += gap }
-            else if(val[i] == "}" && !intext && !incomment) { stage -= gap }
-            if(val[i] == "(" && !intext && !incomment) { /* stage += Math.floor(gap/2) */ }
-            else if(val[i] == ")" && !intext && !incomment) { /* stage -= Math.floor(gap/2) */ }
-            var space = stage  
-            if(incomment == "long") { space = stage+3 }   
-            if(val[i] == "\n") { 
-                  for(var e = i+1; e < val.length; e++) {
-                        if(val[e] != " ") {
-                              res += ("\n" + gotab(space)); i = e-1; break
-                              }
+            var action = !intext && !incomment
+            if(val[i] == "{" && action) { stage += 1 }
+            else if(val[i] == "}" && action) { stage -= 1 }
+            if(val[i] == "(" && action) { quote++ }
+            else if(val[i] == ")" && action) { quote-- }
+            var space = stage*gap
+            if(incomment == "long") { space += left };
+            (function tab() {
+                if(val[i] == "\n" && action) { 
+                    for(var e = i+1; e < val.length; e++) {
+                        if(val[e] != " " || (e) == val.length-1) {
+                            res += ("\n" + (" ").repeat(space)); i = e-1; break
+                            }
                         }
-                  com = true
-                  }    
-            if(com == null) { res += val[i] }
-          //  if(val[i] == ";" && stage == 1 && inht) { res += "\n" } 
+                    com = true
+                    }    
+                else if(val[i] == "}" && val[i+1] != ")" && stage == 1 && action && quote == 1) { 
+                    for(var e = i+1; e <= val.length; e++) {
+                        if( (val[e] != ";" && val[e] != "\n" && val[e] != " ") || (e) == val.length) {
+                            res += ("};\n\n" + (" ").repeat(left)); i = e-1; break
+                            }
+                        };
+                    com = true
+                    }
+                if(com == null) { res += val[i] }
+                })()  
             }
-      if(res[res.length-1] != "\n") { res += "\n" }
-      area.value = res
-      if(stage > 1) { alert(`Expected another ${(stage-1)/gap} symbol(s) "}"`) }
-      };
-    
+        if(val[0] != "\n") { res = "\n" + (" ").repeat(left) + res }
+        if(val[val.length-1] != "\n") { res += "\n" }
+        area.value = res
+        if(stage > 1) { alert(`Expected to add ${(stage-1)} symbol(s) "}"`) }
+        else if(stage < 1) { alert(`Expected to delete ${(stage+1)} symbol(s) "}"`) }
+        if(quote > 1) { alert(`Expected to add ${(qoute-1)} symbol(s) ")"`) }
+        else if(quote < 1) { alert(`Expected to delete ${(quote+1)} symbol(s) ")"`) }
+        };
